abde8a98eb56c1b5116273c2da9bcab4f38aee20
david <dlondono@vertical.com.co>
2024-04-07 18:57:51 -0500

    update

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ system=$(uname -o)
 if [ $system == "GNU/Linux" ]; then
   sh <(curl -L https://nixos.org/nix/install) --daemon
 elif [ $system == "Darwin" ]; then
+  sh <(curl -L https://nixos.org/nix/install)
 elif [ $system == "Android" ]; then
 fi
