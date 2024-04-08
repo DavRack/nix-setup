e2867c590f7e7313d40487e80e61096943036408
david <dlondono@vertical.com.co>
2024-04-08 10:31:06 -0500

    update

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -40,7 +40,7 @@
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  dotfiles = "${home.homeDirectory}/nix-setup/dotfiles"
+  dotfiles = "${home.homeDirectory}/nix-setup/dotfiles";
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
