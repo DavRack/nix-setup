7473c5e144b896035325c7afc9820e8dc1ce3612
david <dlondono@vertical.com.co>
2024-04-07 19:28:00 -0500

    update

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 
 system=$(uname -o)
 
+command=$(curl -V > /dev/null && echo "curl -L" || echo "wget -O -")
+url="https://nixos.org/nix/install"
+
+install_command="$command $url"
+
+
 if [ $system == "GNU/Linux" ]; then
   sh <(curl -L https://nixos.org/nix/install) --daemon
 elif [ $system == "Darwin" ]; then
