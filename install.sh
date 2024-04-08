af863c6ab0a8cb0d3a68e35301eb8bef3d03c42e
david <dlondono@vertical.com.co>
2024-04-07 19:57:01 -0500

    update

--- install.sh
+++ install.sh
@@ -14,7 +14,8 @@ if [ $system == "GNU/Linux" ]; then
 elif [ $system == "Darwin" ]; then
   sh <(eval $install_command)
 fi
-
+bash
 # install home-manager
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
+nix-shell '<home-manager>' -A install
