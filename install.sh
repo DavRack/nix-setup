b7256c32fb5bc6e3fae2d2a057e7a50b23767da0
david_londono <David.Londono@nordstrom.com>
2024-04-07 22:16:09 -0500

    update

--- install.sh
+++ install.sh
@@ -23,3 +23,5 @@ bash
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
+
+ln -s ~/.config/home-manager ~/nix-setup/home-manager
