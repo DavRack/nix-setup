6e517f6daa2d1978e2925b878f3404ac47a243ec
david_londono <David.Londono@nordstrom.com>
2024-10-29 21:43:16 -0500

    update

--- update_home.sh
+++ update_home.sh
@@ -1,22 +1,26 @@
-#/bin/bash
+#!/bin/bash
 
 install_editor=true
-if command -v <the_command> 2>&1 >/dev/null;then
+if command -v vim 2>&1 >/dev/null;then
 	install_editor=false
 fi
 
-if install_editor; then
+if $install_editor; then
+	echo "### installing vim ###"
 	nix-env -iA nixpkgs.vim
 fi
 
 home_modules=$(mktemp)
-ls -1 $HOME/nix-setup/home-manager/ > $home_modules
-vim $home_modules
-MODULES_VALUE=$(cat $home_modules | xargs -n1 realpath | xargs -n1 -I{} \"{}\")
+ls -1 ~/nix-setup/home-manager/ > "$home_modules"
+vim "$home_modules"
+MODULES_VALUE=$(cat "$home_modules" | xargs -n1 realpath | xargs -n1 -I{} \"{}\")
+
+echo "### modules to install ###"
+cat "$home_modules"
 
 MODULES=$MODULES_VALUE home-manager switch -j 16
 
 # cleanup
-if install_editor; then
+if $install_editor; then
 	nix-env --uninstall vim
 fi
