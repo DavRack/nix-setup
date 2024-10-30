9e642c3351811fa426726e1973b0475b383a47f6
david_londono <David.Londono@nordstrom.com>
2024-10-29 22:31:21 -0500

    update

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,8 +7,8 @@
   home.homeDirectory = "/home/david";
 
 	imports = [
-		lib.strings.splitString "\n" (builtins.getEnv "MODULES")
-	]; 
+	]
+	++ lib.strings.splitString "\n" (builtins.getEnv "MODULES");
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
