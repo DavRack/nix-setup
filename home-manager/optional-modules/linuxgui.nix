76fe202956d2563faef49de75843bfd90d37a9fb
david_londono <David.Londono@nordstrom.com>
2025-03-17 20:08:51 -0500

    update

--- home-manager/optional-modules/linuxgui.nix
+++ home-manager/optional-modules/linuxgui.nix
@@ -2,20 +2,23 @@
 {
   home.packages = [
     pkgs.alacritty
-    pkgs.bspwm
-    pkgs.sxhkd
+    # pkgs.bspwm
+    # pkgs.sxhkd
     pkgs.flameshot
+		pkgs.hyprland
+		pkgs.kitty
+		pkgs.uwsm
   ];
 
   home.file = {
-    ".config/bspwm" = {
-      source = ../dotfiles/bspwm;
-      recursive = true;
-    };
+    # ".config/bspwm" = {
+    #   source = ../dotfiles/bspwm;
+    #   recursive = true;
+    # };
 
-    ".config/sxkhd" = {
-      source = ../dotfiles/sxhkd;
-      recursive = true;
-    };
+    # ".config/sxkhd" = {
+    #   source = ../dotfiles/sxhkd;
+    #   recursive = true;
+    # };
   };
 }
